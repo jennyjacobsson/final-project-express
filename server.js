c0b420ef8bcbd84aaec2bef1dabc51736408a458
Jenny Jacobsson <enni@oans.net>
2020-03-06 15:17:54 +0100

    Added post req for ad, and a search query

--- server.js
+++ server.js
@@ -2,27 +2,40 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cloudinary from 'cloudinary'
+import multer from 'multer'
+import cloudinaryStorage from 'multer-storage-cloudinary'
+
+dotenv.config()
+
+cloudinary.config({
+  cloud_name: 'plants', // this needs to be whatever you get from cloudinary
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+const storage = cloudinaryStorage({
+  cloudinary,
+  folder: 'test',
+  allowedFormats: ['jpg', 'png'],
+  transformation: [{ width: 300, height: 300, crop: "limit" }]
+})
+const parser = multer({ storage })
+
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/plantsAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
 const SalesAd = mongoose.model("SalesAd", {
-  type: {
-    type: String,
-  },
-  location: {
-    type: String
-  },
-  description: {
-    type: String
-  },
-  price: {
-    type: Number
-  },
-
-  ///Images somehow???
-
+  imageUrl: String,
+  imageId: String,
+  title: String,
+  type: String,
+  location: String,
+  description: String,
+  price: Number
 })
 
 const port = process.env.PORT || 8080
@@ -38,19 +51,38 @@ app.get('/', (req, res) => {
 })
 
 
-app.post("/ad", async (req, res) => {
+app.post("/ad", parser.single('image'), async (req, res) => {
   try {
     const { type, location, description, price } = req.body
-    const ad = new SalesAd({ type, location, description, price })
+    const ad = new SalesAd({
+      imageUrl: req.file.secure_url,
+      imageId: req.file.public_id,
+      type,
+      location,
+      description,
+      price,
+    })
     const saved = await ad.save()
     res.status(201).json({ saved, message: "Ad completed" })
   } catch (err) {
-    res.status(404).json({ message: "Not found", errors: err.errors })
+    res.status(400).json({ message: "Could not create ad", errors: err.errors })
   }
 })
 
 app.get("/ads", async (req, res) => {
-  res.send(await SalesAd.find())
+  const { search } = req.query
+  const params = {}
+  if (search) {
+    const r = new RegExp(search, 'i')
+    params.$or = [
+      { type: r },
+      { description: r },
+      { location: r }
+    ]
+  }
+
+  const ads = await SalesAd.find(params).limit(30).exec()
+  res.json(ads)
 })
 
 // Start the server
